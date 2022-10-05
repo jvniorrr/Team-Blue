208d92f4e6b1c81f69541d34d8152ba387b92cb3
Junior <49306278+jvniorrr@users.noreply.github.com>
2022-10-04 23:03:25 -0700

    Update AgentRestController.java
    - Return JSON Object relaying information if successful or not.

--- callservice/src/main/java/com/callservice/callservice/Agent/AgentRestController.java
+++ callservice/src/main/java/com/callservice/callservice/Agent/AgentRestController.java
@@ -1,6 +1,7 @@
 package com.callservice.callservice.Agent;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -38,24 +39,42 @@ public class AgentRestController {
 
 
     @PostMapping(value = "/update")
-    public void updateAgent(@RequestBody Map<String, String> info) {
+    public Map<String, String> updateAgent(@RequestBody Map<String, String> info) {
+        Map<String, String> ret = new HashMap<>();
 
         // parse the incoming body request assure proper fields
         String userID = info.get("id");
         String userStatus = info.get("status");
-        // String userStatus = info.get("time");
-        System.out.println(info);
+
         for (SseEmitter emitter : emitters) {
             try {
+                // can return a msg upon successfull call
                 emitter.send(SseEmitter.event().name("updateAgent").data(info));
+                // set return attibutes / keys
+                ret.put("message", "Successfullly processed");
+                ret.put("response", "200");
+                ret.put("success", "true");
+                
             } catch (IOException e) {
+                // TODO: Handle IOException for broken pipes correctly to have a reconnection
+
+                // set return attibutes / keys
+                ret.put("message", "IO Exception");
+                ret.put("response", "500");
+                ret.put("success", "false");
                 System.out.println("IO Exception occured");
+                
             } catch (Exception e) {
-                // TODO: Handle IOException for broken pipes
-                // TODO: handle exception
+                // set return attibutes / keys
+                ret.put("message", "Server Error");
+                ret.put("response", "500");
+                ret.put("success", "false");
+                // handle exception
                 e.printStackTrace();
+
             }
         }
+        return ret;
     }
 }
 
