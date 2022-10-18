b96aecf22fe6a4a10ba4731b22d03dbb06b5accf
Junior <49306278+jvniorrr@users.noreply.github.com>
2022-10-18 11:24:44 -0700

    Update AgentController.java
    - Service handler for frontend page since client is accessing page view frontend.
    - Filter ability on frontend, base bones.

--- callservice/src/main/java/com/callservice/callservice/Agent/AgentController.java
+++ callservice/src/main/java/com/callservice/callservice/Agent/AgentController.java
@@ -1,7 +1,14 @@
 package com.callservice.callservice.Agent;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.callservice.callservice.service.RuntimeProcess;
 
 
 
@@ -10,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AgentController {
+
+    @Autowired
+    private RuntimeProcess service;
 
     public AgentController() {
     }
@@ -24,19 +34,47 @@ public class AgentController {
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model, @RequestParam(name = "status", required = false) String filter) {
+
+        List<Agent> agents;
+        // TODO: add SORT here so dots stay in relative position. Sort by date creation
+        if (filter != null && validFilter(filter))
+        {
+            agents = service.filterAll(filter);
+
+        } 
+        else 
+        {
+            agents = service.readEmployees();
+        }
+
+        model.addAttribute("agents", agents);
+
         return "home";
     }
 
-    // commented out for now, index holds this now
-    // @GetMapping("/agentsPage")
-    // public String agents() {
-    //     return "agents";
-    // }
 
     @GetMapping("/ahmed")
     public String testF() {
         return "ahmedAgents";
+    }
+
+
+
+
+    private Boolean validFilter(String filter) 
+    {
+        if (filter.equalsIgnoreCase("available") ||
+            filter.equalsIgnoreCase("busy") || 
+            filter.equalsIgnoreCase("logged-out") || 
+            filter.equalsIgnoreCase("preview") || 
+            filter.equalsIgnoreCase("after") 
+        ) {
+            return true;
+        } 
+        else {
+            return false;
+        }
     }
 
 }
