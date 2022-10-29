0b12ef28e0eb28243bd176ce0846f969c6b7ed96
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-10-29 09:06:06 -0700

    Resize dots on screen

--- callservice/src/main/resources/static/js/sidebar.js
+++ callservice/src/main/resources/static/js/sidebar.js
@@ -1,8 +1,12 @@
 var menu_btn = document.querySelector("#menu-btn");
 var sidebar = document.querySelector("#sidebar");
 var container = document.querySelector(".my-container");
+var icon = document.querySelector('.fa-chevron-right')
 
 menu_btn.addEventListener("click", () => {
     sidebar.classList.toggle("active-nav");
-    container.classList.toggle("active-cont");
+    menu_btn.classList.toggle("active-cont");
+    icon.classList.toggle("fa-chevron-right");
+    icon.classList.toggle("fa-chevron-left");
+    // container.classList.toggle("active-cont");
 });
