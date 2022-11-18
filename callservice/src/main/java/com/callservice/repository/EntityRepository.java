0f4826d24f27ad7f6ff9665cbf7788b4129cefdc
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-11-17 23:33:28 -0800

    Update EntityRepository.java
    New data entity object handling

--- callservice/src/main/java/com/callservice/repository/EntityRepository.java
+++ callservice/src/main/java/com/callservice/repository/EntityRepository.java
@@ -7,25 +7,14 @@ import java.util.concurrent.CompletableFuture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.lang.Nullable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.callservice.entity.AgentEntity;
 
 
 @Repository
 public interface EntityRepository extends JpaRepository<AgentEntity, Integer> {
-    
-    @Nullable
-    @Query("select max(a.storeId) from AgentEntity a")
-    public CompletableFuture<Integer> findMaxId();
-
-    @Async
-    @Nullable
-    @Query("select max(a.storeId) from AgentEntity a")
-    public CompletableFuture<Integer> findMaxIdAsync();
 
     @Query("select a from AgentEntity a where a.id = ?1")
     public AgentEntity findAgent(String id);
@@ -42,11 +31,18 @@ public interface EntityRepository extends JpaRepository<AgentEntity, Integer> {
     public CompletableFuture<List<AgentEntity>> findAllFilterAsync(String filter);
 
     /** MISC; MAYBE WILL USE */
+
+    // @Async
+    // @Transactional(timeout = 10)
+    // @Override
+    // <S extends AgentEntity> S save(S s);
+
+
     @Async
+    @Transactional(timeout = 10)
     @Modifying
-    @Transactional
-    @Query("update AgentEntity a set a.status = ?1, a.name =?2  where a.id =?3")
-    public CompletableFuture<Integer> updateAgentAsync(String status, String name, String id);
+    @Query(value = "insert into AGENT_TBL(created, id, name, status, updatedTS) values(?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
+    public CompletableFuture<Integer> saveOrUpdate(java.util.Date created, String id, String name, String status, java.util.Date updatedTS);
 
     
 }
