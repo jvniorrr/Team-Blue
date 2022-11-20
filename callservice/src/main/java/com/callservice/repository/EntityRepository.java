8762851426ead387941ce1fb163c00bc71f9853d
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-11-20 12:53:10 -0800

    Update EntityRepository.java

--- callservice/src/main/java/com/callservice/repository/EntityRepository.java
+++ callservice/src/main/java/com/callservice/repository/EntityRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
-import com.callservice.entity.Agent
 import com.callservice.entity.AgentEntity;
 
 
