3aea495e9107f344ed174fb443954f18a21e13ea
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-12-03 13:04:26 -0800

    Statistics page
    - Added new graphs renders for statistics

--- callservice/src/main/resources/static/js/settings.js
+++ callservice/src/main/resources/static/js/settings.js
@@ -35,70 +35,105 @@ let afterStats = parseInt($(".afterStats").html());
 $(document).ready(() => {
     barChart.render();
     circleChart.render();
+    donutChart.render();
 
     let source = new EventSource("/api/v1/init");
 
     // listen for API calls on updates
     source.addEventListener("updateAgent", updateHandler);
+    source.addEventListener("deleteAgent", deleteHandler);
 
-    function updateHandler(event) {
-        let agentData = JSON.parse(event.data);
-
-        if (agentData.status == "available") {
-            $(".availableStats").html(++availableStats);
-        } else if (agentData.status == "busy") {
-            $(".busyStats").html(++busyStats);
-        } else if (agentData.status == "preview") {
-            $(".previewStats").html(++previewStats);
-        } else if (
-            agentData.status == "loggedout" ||
-            agentData.status == "logged-out"
-        ) {
-            $(".loggedOutStats").html(++loggedOutStats);
-        } else if (agentData.status == "after") {
-            $(".afterStats").html(++afterStats);
-        }
-
-        $(".entityStats").html(++entityStats);
-
-        barChart.updateSeries([
-            {
-                name: "Status",
-                data: [
-                    {
-                        x: "Available",
-                        y: availableStats,
-                        fillColor: agentAvailableColor,
-                    },
-                    {
-                        x: "Busy",
-                        y: busyStats,
-                        fillColor: agentBusyColor,
-                    },
-                    {
-                        x: "Preview",
-                        y: previewStats,
-                        fillColor: agentPreviewColor,
-                    },
-                    {
-                        x: "Logged Out",
-                        y: loggedOutStats,
-                        fillColor: agentLoggedOutColor,
-                    },
-                    {
-                        x: "After",
-                        y: afterStats,
-                        fillColor: agentAfterColor,
-                    },
-                ],
-            },
-        ]);
-
-        circleChart.updateSeries([
-            (availableStats / entityStats) * 100, (busyStats / entityStats) * 100 , (previewStats / entityStats) * 100, (loggedOutStats / entityStats) * 100, (afterStats / entityStats) * 100,
-        ]);
-    }
+    
 });
+
+function updateHandler(event) {
+    let agentData = JSON.parse(event.data);
+
+    if (agentData.status == "available") {
+        $(".availableStats").html(++availableStats);
+    } else if (agentData.status == "busy") {
+        $(".busyStats").html(++busyStats);
+    } else if (agentData.status == "preview") {
+        $(".previewStats").html(++previewStats);
+    } else if (
+        agentData.status == "loggedout" ||
+        agentData.status == "logged-out"
+    ) {
+        $(".loggedOutStats").html(++loggedOutStats);
+    } else if (agentData.status == "after") {
+        $(".afterStats").html(++afterStats);
+    }
+
+    $(".entityStats").html(++entityStats);
+    updateCharts();
+
+    
+}
+
+function deleteHandler(event) {
+    let data = JSON.parse(event.data);
+
+    if (data.status == "available") {
+        $(".availableStats").html(--availableStats);
+    } else if (data.status == "busy") {
+        $(".busyStats").html(--busyStats);
+    } else if (data.status == "preview") {
+        $(".previewStats").html(--previewStats);
+    } else if (
+        data.status == "loggedout" ||
+        data.status == "logged-out"
+    ) {
+        $(".loggedOutStats").html(--loggedOutStats);
+    } else if (data.status == "after") {
+        $(".afterStats").html(--afterStats);
+    }
+
+    $(".entityStats").html(--entityStats);
+    updateCharts();
+}
+
+let updateCharts = () => {
+    barChart.updateSeries([
+        {
+            name: "Status",
+            data: [
+                {
+                    x: "Available",
+                    y: availableStats,
+                    fillColor: agentAvailableColor,
+                },
+                {
+                    x: "Busy",
+                    y: busyStats,
+                    fillColor: agentBusyColor,
+                },
+                {
+                    x: "Preview",
+                    y: previewStats,
+                    fillColor: agentPreviewColor,
+                },
+                {
+                    x: "Logged Out",
+                    y: loggedOutStats,
+                    fillColor: agentLoggedOutColor,
+                },
+                {
+                    x: "After",
+                    y: afterStats,
+                    fillColor: agentAfterColor,
+                },
+            ],
+        },
+    ]);
+
+    circleChart.updateSeries([
+        (availableStats / entityStats) * 360, (busyStats / entityStats) * 360 , (previewStats / entityStats) * 360, (loggedOutStats / entityStats) * 360, (afterStats / entityStats) * 360,
+    ]);
+    
+    donutChart.updateSeries([
+        (availableStats / entityStats), (busyStats / entityStats), (previewStats / entityStats), (loggedOutStats / entityStats), (afterStats / entityStats),
+    ]);
+}
 
 // Default Chart Options
 const defaultOptions = {
@@ -113,7 +148,7 @@ const defaultOptions = {
             enabled: false,
         },
         width: "100%",
-        height: 100,
+        height: 380,
         offsetY: 8,
     },
     stroke: {
@@ -134,12 +169,11 @@ const defaultOptions = {
     },
 };
 
-// Bar Chart
+// Bar Chart -- Credit @https://github.com/frontend-joe/es6-charts/tree/main/bar-chart
 let barOptions = {
     ...defaultOptions,
     chart: {
-        height: 380,
-        // ...defaultOptions.chart,
+        ...defaultOptions.chart,
         type: "bar",
     },
     tooltip: {
@@ -217,33 +251,124 @@ let barOptions = {
 };
 
 
-
 let circleOptions = {
     chart: {
         height: 380,
         type: "radialBar",
+        animations: {
+            speed: 900,
+            animateGradually: {
+                enabled: true,
+                delay: 150,
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350,
+            }
+        }
     },
     plotOptions: {
         radialBar: {
             dataLabels: {
-                showOn: "hover",
+                showOn: "always",
                 value: {
                     color: "#fff",
                     formatter: (val) => {
-                        return val.substring(0, 2) + "%";
+                        let val2 = (val/365) * 100;
+                        return val2.toString().substring(0, 2) + "%";
                     }, 
                 },
-
             },
+            
         },
     },
+    // stroke: {
+    //     lineCap: "round"
+    // },
     series: [
-        (availableStats / entityStats) * 100, (busyStats / entityStats) * 100 , (previewStats / entityStats) * 100, (loggedOutStats / entityStats) * 100, (afterStats / entityStats) * 100,
+        (availableStats / entityStats) * 360, (busyStats / entityStats) * 360 , (previewStats / entityStats) * 360, (loggedOutStats / entityStats) * 360, (afterStats / entityStats) * 360,
+        // (availableStats / entityStats) * 100, (busyStats / entityStats) * 100 , (previewStats / entityStats) * 100,
     ],
-    labels: ["After", "Logged Out", "Preview", "Busy", "Available"],
+    colors: [ agentAvailableColor, agentBusyColor, agentPreviewColor, agentLoggedOutColor, agentAfterColor ],
+    labels: ["Available", "Busy", "Preview", "Logged Out", "After"],
 };
 
+let donutOptions = {
+    series: [
+        (availableStats / entityStats), (busyStats / entityStats), (previewStats / entityStats), (loggedOutStats / entityStats), (afterStats / entityStats),
+    ],
+    colors: [ agentAvailableColor, agentBusyColor, agentPreviewColor, agentLoggedOutColor, agentAfterColor ],
+    labels: ["Available", "Busy", "Preview", "Logged Out", "After"],
+    dataLabels: {
+        enabled: true,
+        formatter: (val, opts) => {
+            return val.toString().substring(0,2) + "%";
+        },
+    },
+    legend: {
+        labels: {
+            colors: [ "#fff" ],
+            // userSeriesColors: true,
+        },
+    },
+    stroke: {
+        show: true,
+        curve: "smooth",
+        width: 1,
+        colors: ["#000"],
+    },
+    chart: {
+        height: 380,
+        type: "donut",
+        animations: {
+            speed: 800,
+            animateGradually: {
+                enabled: true,
+                delay: 150,
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350,
+            }
+        }
+    },
+    tooltip: {
+        fillSeriesColor: false,
+        y: {
+            formatter: (val) => {
+                return (val * 100).toString().substring(0,5) + "%";
+            }
+        }
+    },
+    plotOptions: {
+        pie: {
+            donut: {
+                show: true,
+                labels: {
+                    value: {
+                        show: true,
+                        formatter: (val) => {
+                            return val.toString().substring(0,4) + "%";
+                        }
+                    },
+                },
+            },
+            expandOnClick: true,
+            
+        }
+    }
+}
 
+/**
+ * Function to update the color choices agent wants
+ */
+function updateSettings() {
+    let newSettings = {
+        available: "#",
+    }
+}
 
 var circleChart = new ApexCharts(document.querySelector("#circle-chart"), circleOptions);
 var barChart = new ApexCharts(document.querySelector("#bar-chart"), barOptions);
+var donutChart = new ApexCharts(document.querySelector("#donut-chart"), donutOptions);
+
