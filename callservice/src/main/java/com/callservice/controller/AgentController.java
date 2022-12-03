86ded8ff84e1a4fbe56177926daefdf3ef123396
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-12-02 20:12:26 -0800

    Custom error page
    - Custom error page if unknown route is provided

--- callservice/src/main/java/com/callservice/controller/AgentController.java
+++ callservice/src/main/java/com/callservice/controller/AgentController.java
@@ -3,10 +3,14 @@ package com.callservice.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -27,7 +31,7 @@ import com.callservice.utils.ValidatorHelper;
  * Controller to handle rendering web pages using Thymeleaf; essentially though pure html css and js.
  */
 @Controller
-public class AgentController {
+public class AgentController implements ErrorController {
 
     Logger logger = LoggerFactory.getLogger(AgentController.class);
 
@@ -91,5 +95,20 @@ public class AgentController {
         logger.info("Page has agents " + agents.size() + " agents");
         logger.info("Returning index page");
         return "index";
+    }
+
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        // if (status != null) {
+        //     int statusCode = Integer.parseInt(status.toString());
+
+            
+        // }
+
+        return "404";
     }
 }
